626fb93bce7d25fa77dac9b8f599aaa47ca430cc
Madis Nõmme <madis.nomme@gmail.com>
2013-10-02 22:11:03 +0300

    Add link to root url

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -1,17 +1,27 @@
 require 'spec_helper'
 
-describe "landing page" do
-  it "shows company name" do
-    visit '/'
-    within('body') { expect(page).to have_content('CodeCrowd') }
+describe "Pages" do
+
+  describe "home page" do
+    it "shows company name" do
+      visit '/'
+      within('body') { expect(page).to have_content('CodeCrowd') }
+    end
   end
 
-  it "links to showroom"
+  describe "some page" do
+    let!(:about_page) { Page.create(title: 'About', content: 'This is CodeCrowd', slug: 'about') }
 
-  it "links to about" do
-    Page.create(title: 'About', content: 'This is CodeCrowd', slug: 'about')
-    visit '/'
-    click_link 'About'
-    expect(current_url).to eq(slug_url('about'))
+    it "links to about" do
+      visit '/'
+      click_link 'About'
+      expect(current_url).to eq(slug_url('about'))
+    end
+
+    it "has link back to root" do
+      visit '/about'
+      expect(find_link('CodeCrowd')[:href]).to eq '/'
+    end
+
   end
 end
